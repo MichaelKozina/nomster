814b0377c7a1e09235441b815965c82e47365be1
Michael Kozina <mike.kozina@gmail.com>
2019-03-07 20:48:49 +0000

    commit35

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'email.mailer.test@gmail.com'
   layout 'mailer'
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   
   def comment_added
-    mail(to: "mike.kozina@gmail.com",
+    mail(to: "email.mailer.test@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
