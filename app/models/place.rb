6246366ecff4af9f7da1cb51637c91c092297046
Michael Kozina <mike.kozina@gmail.com>
2019-03-07 15:34:12 +0000

    commit23

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  validates :name, :address, :description, presence: true
+  validates :name, length: { minimum: 4 }
+  geocoded_by :address
+  after_validation :geocode
   validates :name, presence: true
 end
