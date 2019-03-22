e2becfd181d76d03ded203cb4741f85d77159ee4
Michael Kozina <mike.kozina@gmail.com>
2019-03-22 15:55:04 +0000

    commit52

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,8 +1,14 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
   #storage :file
   #storage :aws
-  storage :fog
+  #storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
